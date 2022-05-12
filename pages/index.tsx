8f704d4b62b6466e04a396b7903d300624fa7a6c
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-13 00:11:31 +0700

    feat: add lootbox canvas to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,26 @@
 import type { NextPage } from 'next';
 import { Header } from 'components/Header';
+import { LootboxCanvas } from 'canvas';
+import { LoadingIndicator } from 'components/LoadingIndicator';
 
 const Home: NextPage = () => {
   return (
     <div>
       <Header />
-      <div className="border border-red-500 p-4 mt-4">This is main</div>
-      <h1>test vercel 2</h1>
+      <div className="centered">
+        <div className="grid grid-cols-2 ">
+          <div className="border border-red-400">
+            <h1 className="text-[200px] -mb-5 -ml-3">Pandora</h1>
+            <h3 className="text-2xl">
+              Decentralized NFT loot box. Gamify NFT buy-and-sell experiences to solve NFT market's
+              illiquidity problems.
+            </h3>
+          </div>
+          <div className="h-100 ">
+            <LootboxCanvas />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
