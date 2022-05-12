1bf7f2e6ded64812f4548dca5ac45509e482546f
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-13 01:26:54 +0700

    fix: Page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,15 +8,15 @@ const Home: NextPage = () => {
     <div>
       <Header />
       <div className="centered">
-        <div className="grid grid-cols-2 ">
-          <div className="border border-red-400">
-            <h1 className="text-[200px] -mb-5 -ml-3">Pandora</h1>
+        <div className="grid grid-cols-2">
+          <div className="ml-24 mt-9">
+            <h1 className="text-[180px] -mb-5 -ml-3">Pandora</h1>
             <h3 className="text-2xl">
               Decentralized NFT loot box. Gamify NFT buy-and-sell experiences to solve NFT market's
               illiquidity problems.
             </h3>
           </div>
-          <div className="h-100 ">
+          <div className="h-100">
             <LootboxCanvas />
           </div>
         </div>
