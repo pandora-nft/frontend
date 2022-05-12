5614374700eeb30039a9f1a81b29dd5ad4da0439
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-12 23:00:01 +0700

    chore: moralis credentials

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
       <nav className="p-5 border-b-2 flex flex-row">
         <h1 className="py-4 px-4 font-bold text-3xl">Pandora</h1>
         <div className="ml-auto py-2 px-4">
-          <ConnectButton moralisAuth={false} />
+          <ConnectButton moralisAuth={true} signingMessage="Moralis Authentication" />
         </div>
       </nav>
     </>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import 'styles/globals.css';
 import type { AppProps } from 'next/app';
 import { MoralisProvider } from 'react-moralis';
+import { isMoralisEnvProvided, MORALIS_APP_ID, MORALIS_SERVER_URL } from 'config';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MoralisProvider initializeOnMount={false}>
+    <MoralisProvider
+      serverUrl={MORALIS_SERVER_URL}
+      appId={MORALIS_APP_ID}
+      initializeOnMount={isMoralisEnvProvided}
+    >
       <Component {...pageProps} />
     </MoralisProvider>
   );
