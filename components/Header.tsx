c2bf1dc261f1e11fc10a31d7d28ed52162805e4f
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-13 01:28:08 +0700

    feat: add nav link name

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,10 +3,15 @@ import { ConnectButton } from 'web3uikit';
 export const Header = () => {
   return (
     <>
-      <nav className="p-5 border-b-2 flex flex-row">
+      <nav className="p-5 flex flex-row justify-between">
         <h1 className="py-4 px-4 font-bold text-3xl">Pandora</h1>
-        <div className="ml-auto py-2 px-4">
-          <ConnectButton moralisAuth={false} />
+        <div className="flex flex-row justify-between">
+          <div className="pt-5 pr-5">Home</div>
+          <div className="pt-5 pr-5">Market</div>
+          <div className="pt-5 pr-5">Whitepaper</div>
+        </div>
+        <div className="py-2 px-4">
+          <ConnectButton moralisAuth={true} signingMessage="Moralis Authentication" />
         </div>
       </nav>
     </>
