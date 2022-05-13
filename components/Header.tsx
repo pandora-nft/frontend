d2abb1fa7d1e60c24931610c9f1cf9ce960c2003
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-13 19:37:04 +0700

    fix: header font size and weight

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,14 +2,14 @@ import { ConnectButton } from 'web3uikit';
 
 export const Header = () => {
   const createNavLink = (label: string) => {
-    return <div className="pt-5 pr-5">{label}</div>;
+    return <div className="font-light text-lg pt-5 pr-5">{label}</div>;
   };
   return (
     <>
-      <nav className="p-5 flex flex-row items-center justify-between border-b border-gray-200">
+      <nav className="p-5 flex flex-row items-center justify-between border-gray-200">
         <div className="flex flex-row items-center justify-center">
           <img className="w-8 h-8" src={'box.png'} alt="icon" />
-          <div className="text-3xl pt-[24px] pl-2">Pandora</div>
+          <div className="text-3xl font-bold pt-[2px] pl-2">Pandora</div>
         </div>
         <div className="flex flex-row justify-between">
           {createNavLink('Home')}
