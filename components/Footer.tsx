cd16e074a88dca8a1189c73638aa4662a757a843
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-05-14 13:46:10 +0700

    feat: add router link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,10 @@
 export const Footer = () => {
-  const createNavLink = (label: string) => {
-    return <div className="pt-5 pr-5 font-light text-gray-500">{label}</div>;
+  const createNavLink = (label: string, link: string) => {
+    return (
+      <a href={`./${link}`} className="pt-5 pr-5 font-light text-gray-500">
+        {label}
+      </a>
+    );
   };
 
   return (
@@ -8,10 +12,11 @@ export const Footer = () => {
       <div className="w-full h-[1px] border-t border-gray-200 mb-20"></div>
       <h1 className="text-gray-500 font-medium">Pandora</h1>
       <div className="flex flex-row justify-between">
-        {createNavLink('Home')}
-        {createNavLink('Marketplace')}
-        {createNavLink('Roadmap')}
-        {createNavLink('Whitepaper')}
+        {createNavLink("Home", "")}
+        {createNavLink("Marketplace", "")}
+        {createNavLink("Create", "create")}
+        {createNavLink("Roadmap", "")}
+        {createNavLink("Whitepaper", "")}
       </div>
     </div>
   );
