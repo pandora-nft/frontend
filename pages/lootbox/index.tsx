135bb5a05ada9f4177334256b1b5f69a22da466a
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-05-14 23:51:50 +0700

    feat: create dynamic routing lootbox page

---
+++ pages/lootbox/index.tsx
@@ -0,0 +1,18 @@
+// pages/lootbox/[bid].js
+
+import { useState } from "react";
+import Router, { useRouter } from "next/router";
+
+import { ConnectButton } from "web3uikit";
+import Link from "next/link";
+import Image from "next/image";
+const Lootbox = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      <div>Loot box index</div>
+    </>
+  );
+};
+export default Lootbox;
