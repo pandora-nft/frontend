f8326f3a86167d4c9f6d223cbdcdbb70bfd78c85
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-18 01:20:49 +0700

    fix: fetchNFTs

--- hooks/useLootbox.ts
+++ hooks/useLootbox.ts
@@ -1,13 +1,14 @@
-import { LOOTBOX_ABI } from "contract"
-import { useMoralis, useChain } from "react-moralis"
+import { LOOTBOX_ABI, TICKET_ADDRESS } from "contract"
+import { useMoralis, useChain, useMoralisWeb3Api } from "react-moralis"
 import { useState } from "react"
 import { ethers } from "ethers"
 import { Lootbox, NFT } from "types"
-import { getNFTMetadata } from "api"
 
 export const useLootbox = () => {
   const { web3: moralisProvider } = useMoralis()
   const { chain } = useChain()
+
+  const Web3Api = useMoralisWeb3Api()
 
   const [lootbox, setLootbox] = useState<Lootbox>({
     address: "",
@@ -18,13 +19,18 @@ export const useLootbox = () => {
   const fetchLootbox = async (lootboxAddress: string) => {
     const lootboxContract = new ethers.Contract(lootboxAddress, LOOTBOX_ABI, moralisProvider)
     const fetchNfts = await lootboxContract.getAllNFTs()
+    console.log("fetchNFTs", lootboxAddress, fetchNfts)
 
     let nfts: NFT[] = []
     for (let nft of fetchNfts) {
-      const nftAddress = nft.address.toString()
-      const tokenId = +nft.tokenId.toString()
+      const nftAddress = nft._address.toString()
+      const tokenId = +nft._tokenId.toString()
 
-      const nftMetadata = await getNFTMetadata(chain.networkId, nftAddress, tokenId)
+      // const nftMetadata = await getNFTMetadata(chain.networkId, nftAddress, tokenId)
+      const nftMetadata = await Web3Api.token.getNFTMetadata({
+        address: TICKET_ADDRESS[chain.networkId],
+        chain: "rinkeby",
+      })
       console.log("nft metadata", nftMetadata)
 
       nfts.push({ tokenId, address: nftAddress, imageURI: "" })
