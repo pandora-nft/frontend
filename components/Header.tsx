defae9f2e4d7dba5a0da3226c25f90f8ec2ce877
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-05-17 20:45:12 +0700

    fix: update multichain address

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,13 +14,13 @@ export const Header = () => {
       <nav className="p-5 flex flex-row items-center justify-between border-gray-200">
         <div className="flex flex-row items-center justify-center">
           <img className="w-8 h-8" src={"/box.png"} alt="icon" />
-          <div className="text-3xl font-bold pt-[2px] pl-2">{createNavLink("Pandora", "")}</div>
+          <div className="text-3xl font-bold pt-[2px] pl-2">{createNavLink("Pandora", "/")}</div>
         </div>
         <div className="flex flex-row justify-between">
           {createNavLink("Home", "/")}
           {createNavLink("Marketplace", "/marketplace")}
           {createNavLink("Create", "/create")}
-          {createNavLink("Lootbox", "/lootbox/0x00000001")}
+          {createNavLink("Lootbox", "/lootbox/0")}
           {createNavLink("Roadmap", "/")}
           {createNavLink("Whitepaper", "/")}
         </div>
