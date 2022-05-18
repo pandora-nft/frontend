09974c5f42ffcb43069fe9f4bd279adffabdefe0
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-19 01:29:08 +0700

    feat: profile/lootbox pages

--- pages/profile/lootbox.tsx
+++ pages/profile/lootbox.tsx
@@ -1,8 +1,36 @@
+import { useLootboxFactory } from "hooks"
 import ProfileLayout from "layouts/profileLayout"
-import { ReactElement } from "react"
+import { ReactElement, useEffect } from "react"
+import { LootboxCard } from "components"
+import { useMoralis } from "react-moralis"
 
 const Lootbox = () => {
-  return <>This is profile/lootbox</>
+  const { enableWeb3, isWeb3Enabled, account } = useMoralis()
+  const { lootboxOwned, fetchLootboxOwned } = useLootboxFactory()
+
+  useEffect(() => {
+    if (isWeb3Enabled) {
+      fetchLootboxOwned(account)
+    } else {
+      enableWeb3()
+    }
+  }, [isWeb3Enabled])
+  return (
+    <>
+      This is profile/lootbox
+      <div className="centered mt-10">
+        <div className="grid grid-cols-2 gap-10 border-black p-4">
+          {lootboxOwned.map((lootbox) => {
+            return (
+              <>
+                <LootboxCard lootbox={lootbox} />
+              </>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  )
 }
 
 Lootbox.getLayout = function getLayout(page: ReactElement) {
