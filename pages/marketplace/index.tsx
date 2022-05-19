ad6f9dbb1534d19032664576d9ba30107c10a215
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-05-19 17:00:43 +0700

    fix: resposive marketplace

--- pages/marketplace/index.tsx
+++ pages/marketplace/index.tsx
@@ -7,7 +7,7 @@ const Marketplace = () => {
   return (
     <div className="centered mt-10">
       <h2 className="font-medium mb-10">Marketplace</h2>
-      <div className="grid grid-cols-2 gap-10 border-black p-4">
+      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-6 pt-6 border-black p-4">
         {allLootboxes.map((lootbox) => {
           return (
             <>
