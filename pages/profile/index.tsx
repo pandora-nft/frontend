204d1c934a49c24e05bd97de9c45c0af3bc4cdf4
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-05-19 17:01:23 +0700

    feat: add NFT balance to profile

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 const Profile = () => {
   return (
     <>
-      <div className="centered mt-10">
+      <div className="centered mt-10 ">
         <h2 className="font-medium mb-10">Profile index</h2>
         <div className="grid grid-cols-2 gap-10 border-black p-4"></div>
         <Link href="/profile/nft">Go to profile/nft </Link>
--- pages/profile/nft.tsx
+++ pages/profile/nft.tsx
@@ -1,8 +1,37 @@
 import ProfileLayout from "layouts/profileLayout"
 import { ReactElement } from "react"
-
+import { useNFTsBalance } from "hooks"
+import { NFTCard } from "components/NFTs/NFTCard"
 const Nft = () => {
-  return <>This is profile/nft</>
+  const { NFTBalances } = useNFTsBalance()
+  console.log(NFTBalances)
+  return (
+    <>
+      <div className="container mx-auto max-w-4/5 min-w-sm">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 pt-6">
+          {NFTBalances ? (
+            NFTBalances.result.map((nft, index) => {
+              return (
+                <NFTCard
+                  key={index}
+                  NFT={{
+                    name: nft.metadata.name,
+                    collectionName: nft.name,
+                    description: nft.metadata.description,
+                    tokenId: nft.token_id,
+                    address: nft.token_address,
+                    imageURI: nft.image,
+                  }}
+                ></NFTCard>
+              )
+            })
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+    </>
+  )
 }
 
 Nft.getLayout = function getLayout(page: ReactElement) {
