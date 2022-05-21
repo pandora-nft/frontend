1f0173dd35cb7e8a2afab588777c810d6f95cce2
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-21 15:48:08 +0700

    feat: empty pic for  0 nft lootbox

--- components/Lootbox/LootboxCard.tsx
+++ components/Lootbox/LootboxCard.tsx
@@ -6,11 +6,42 @@ interface LootboxCardProps {
 }
 
 export const LootboxCard: React.FC<LootboxCardProps> = ({ lootbox }) => {
-  const { name, address, ticketPrice, ticketSold, nfts, drawTimestamp } = lootbox
+  const { id, name, address, ticketPrice, ticketSold, nfts, drawTimestamp } = lootbox
 
+  const showNFTs = () => {
+    if (nfts.length < 4) {
+      return (
+        <>
+          {Array(4)
+            .fill(null)
+            .map((_, index) => {
+              return (
+                <div key={index} className="w-48 mr-5">
+                  <img className="rounded w-[110px] h-[100px]" src="EmptyPic.png" />
+                </div>
+              )
+            })}
+        </>
+      )
+    } else {
+      return (
+        <>
+          {nfts.map((nft, index) => {
+            return (
+              <div key={index} className="w-48 mr-5">
+                <img className="rounded" src={nft.imageURI} />
+              </div>
+            )
+          })}
+        </>
+      )
+    }
+  }
   return (
-    <div className="rounded border border-gray-800 shadow-xl p-10 flex flex-col hover:scale-[101%] transition duration-500">
-      <h2 className="text-left font-medium"># {name}</h2>
+    <div className="rounded border border-gray-800 shadow-xl h-[350px] p-10 flex flex-col hover:scale-[101%] transition duration-500">
+      <h2 className="text-left font-medium">
+        #{id} {name}
+      </h2>
       <h3 className="text-left font-medium mb-5">{address}</h3>
 
       <LootboxDetail
@@ -19,15 +50,7 @@ export const LootboxCard: React.FC<LootboxCardProps> = ({ lootbox }) => {
         ticketSold={ticketSold}
         drawTimestamp={drawTimestamp}
       />
-      <div className="flex flex-row justify-between p-5">
-        {nfts.map((nft, index) => {
-          return (
-            <div key={index} className="w-48 mr-5">
-              <img className="rounded" src={nft.imageURI} />
-            </div>
-          )
-        })}
-      </div>
+      <div className="flex flex-row justify-between p-5">{showNFTs()}</div>
     </div>
   )
 }
