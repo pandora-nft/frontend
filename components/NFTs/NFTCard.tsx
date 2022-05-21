3a7b15f25ca8744e8d67b26bce92c5334a0880a4
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-05-22 01:35:12 +0700

    fix: restyling NFT Card

--- components/NFTs/NFTCard.tsx
+++ components/NFTs/NFTCard.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import Link from "next/link"
 import { NFT } from "types"
 
 interface NFTCardProps {
@@ -7,24 +6,40 @@ interface NFTCardProps {
 }
 
 export const NFTCard: React.FC<NFTCardProps> = ({ NFT }) => {
-  const { name, description, collectionName, tokenId, address, imageURI } = NFT
+  const { name, description, collectionName, tokenId, imageURI } = NFT
 
   return (
-    <Link href={`https://testnets.opensea.io/assets/rinkeby/${address}/${tokenId}`}>
-      <div className="grid grid-flow-row min-h-64 rounded border border-gray-400 cursor-pointer bg-gray-25 transform  hover:scale-[103%] transition duration-300">
-        <img className="h-full rounded-xl" src={imageURI || "error"} alt="" />
-        <div className="w-full h-fit min-h-20 border-t border-gray-400 text-left pt-2 pl-2">
-          <p className=" font-medium ">{name}</p>
-          <p className=" font-normal text-sm ">
-            {collectionName}#{tokenId}{" "}
-          </p>
+    <div className="grid grid-flow-row w-full h-full rounded border border-gray-400 cursor-pointer bg-gray-25 transform  hover:scale-[103%] transition duration-300">
+      <img
+        className="block object-cover object-center w-full h-full rounded-lg"
+        src={imageURI || "error"}
+        alt=""
+      />
+      <div className="pt-2 pl-2 border-t border-gray-400 text-left max-h-fit">
+        <h5 className="break-words font-medium ">{name}</h5>
+        <p className="text-gray-700 font-medium text-medium mb-2">
+          {collectionName} #{tokenId}{" "}
+        </p>
+        <p className=" font-medium text-sm">
+          {description?.length > 40 ? <>{description.substring(0, 40)}...</> : description}
+        </p>
+      </div>
+
+      {/* <div className="w-full h-fit min-h-20 border-t border-gray-400 text-left pt-2 pl-2">
+        <p className="break-words font-medium ">{name}</p>
+        <p className="break-words font-normal text-sm ">
+          {collectionName}#{tokenId}{" "}
+        </p>
+        {description ? (
           <div className=" font-medium text-sm">
             {description?.length > 40 ? <>{description.substring(0, 40)}...</> : description}
           </div>
-        </div>
+        ) : (
+          <></>
+        )}
+      </div> */}
 
-        {/* <span>{imageURI} </span> */}
-      </div>
-    </Link>
+      {/* <span>{imageURI} </span> */}
+    </div>
   )
 }
