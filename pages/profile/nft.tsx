8358df859dde7bd8963a3f530fce6b77bf2b40e6
pakkaponwattanawaha <54467698+pakkaponwattanawaha@users.noreply.github.com>
2022-05-22 19:41:15 +0700

    fix: syntax fix

--- pages/profile/nft.tsx
+++ pages/profile/nft.tsx
@@ -15,35 +15,39 @@ const Nft = () => {
     } else if (!NFTBalances) {
       return <h2>You have no NFTs</h2>
     } else {
-
-  return (
-    <>
-      <div className="container mx-auto max-w-4/5 min-w-sm">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 pt-6">
-          {NFTBalances ? (
-            NFTBalances.result.map((nft, index) => {
-              const NFT = {
-                name: nft?.metadata?.name,
-                collectionName: nft?.name,
-                description: nft?.metadata?.description,
-                tokenId: nft?.token_id,
-                address: nft?.token_address,
-                imageURI: nft?.image,
-              }
-              return (
-                <a
-                  key={index}
-                  href={`https://testnets.opensea.io/assets/rinkeby/${NFT.address}/${NFT.tokenId}`}
-                >
-                  <NFTCard NFT={NFT} />
-                </a>
-              )
-            )
-          })}
-        </>
-      )
+      return (
+        <>
+          <div className="container mx-auto max-w-4/5 min-w-sm">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 pt-6">
+              {NFTBalances ? (
+                NFTBalances.result.map((nft, index) => {
+                  const NFT = {
+                    name: nft?.metadata?.name,
+                    collectionName: nft?.name,
+                    description: nft?.metadata?.description,
+                    tokenId: nft?.token_id,
+                    address: nft?.token_address,
+                    imageURI: nft?.image,
+                  }
+                  return (
+                    <a
+                      key={index}
+                      href={`https://testnets.opensea.io/assets/rinkeby/${NFT.address}/${NFT.tokenId}`}
+                    >
+                      <NFTCard NFT={NFT} />
+                    </a>
+                  )
+                  }
+                  )
+                )
+              :<></>}
+              </div>
+              </div>
+            </>
+          )
+  
     }
-  }
+  
 
   return (
     <>
