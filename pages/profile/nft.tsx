fc7bcdfb2c409c7f14f378f233180b93b0d196e6
pakkaponwattanawaha <54467698+pakkaponwattanawaha@users.noreply.github.com>
2022-05-22 19:48:55 +0700

    fix: resolve merge conflict

--- pages/profile/nft.tsx
+++ pages/profile/nft.tsx
@@ -2,10 +2,8 @@ import ProfileLayout from "layouts/profileLayout"
 import { ReactElement } from "react"
 import { useNFTsBalance, useSkeleton } from "hooks"
 import { NFTCard, NFTCardSkeleton } from "components"
-
 const Nft = () => {
   const { NFTBalances, isLoading } = useNFTsBalance()
-
 
   const { showSkeleton } = useSkeleton()
 
@@ -17,38 +15,28 @@ const Nft = () => {
     } else {
       return (
         <>
-          <div className="container mx-auto max-w-4/5 min-w-sm">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 pt-6">
-              {NFTBalances ? (
-                NFTBalances.result.map((nft, index) => {
-                  const NFT = {
-                    name: nft?.metadata?.name,
-                    collectionName: nft?.name,
-                    description: nft?.metadata?.description,
-                    tokenId: nft?.token_id,
-                    address: nft?.token_address,
-                    imageURI: nft?.image,
-                  }
-                  return (
-                    <a
-                      key={index}
-                      href={`https://testnets.opensea.io/assets/rinkeby/${NFT.address}/${NFT.tokenId}`}
-                    >
-                      <NFTCard NFT={NFT} />
-                    </a>
-                  )
-                  }
-                  )
-                )
-              :<></>}
-              </div>
-              </div>
-            </>
-          )
-  
+          {NFTBalances.result.map((nft, index) => {
+            return (
+              nft.metadata && (
+                <div key={index}>
+                  <NFTCard
+                    NFT={{
+                      name: nft.metadata.name,
+                      collectionName: nft.name,
+                      description: nft.metadata.description,
+                      tokenId: nft.token_id,
+                      address: nft.token_address,
+                      imageURI: nft.image,
+                    }}
+                  />
+                </div>
+              )
+            )
+          })}
+        </>
+      )
     }
-  
-
+  }
   return (
     <>
       <div className="container mx-auto max-w-4/5 min-w-sm">
@@ -59,9 +47,7 @@ const Nft = () => {
     </>
   )
 }
-
 Nft.getLayout = function getLayout(page: ReactElement) {
   return <ProfileLayout>{page}</ProfileLayout>
 }
-
 export default Nft
