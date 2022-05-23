d199768a1a5be14d5608d33c06cb34bca9543fc5
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-23 18:46:05 +0700

    fix: css nft profile

--- pages/profile/nft.tsx
+++ pages/profile/nft.tsx
@@ -39,8 +39,11 @@ const Nft = () => {
   }
   return (
     <>
-      <div className="container mx-auto max-w-4/5 min-w-sm">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 pt-6">
+      <div className="container mt-10 mx-auto max-w-4/5 min-w-sm">
+        <div
+          className="grid grid-cols-2 lg:grid-cols-3
+                     xl:grid-cols-4 3xl:grid-cols-5 5xl:grid-cols-6 gap-5"
+        >
           {showNFTs()}
         </div>
       </div>
