5e7d47ebd459194bd1a45da50627dddd02ed333c
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-28 04:32:11 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { LootboxCanvas, NFTShowcase } from "canvas"
 import Tilt from "react-parallax-tilt"
 import { AnimationOnScroll } from "react-animation-on-scroll"
 import "animate.css/animate.min.css"
+import { openInNewTab } from "utils"
 import { Card } from "components"
 import { useRouter } from "next/router"
 import { Illustration } from "web3uikit"
@@ -15,25 +16,37 @@ const Home: NextPage = () => {
         <div className="grid grid-cols-2 px-10 pt-10">
           <div className="mt-10 text-left">
             <div className="text-[10rem] font-medium -mb-16 -ml-3">Pandora</div>
-            <h2 className="font-[100]">
+            <h2 className="font-[100]  border-red-500 w-110">
               The decentralized NFT loot box gamifying trading experiences to solve NFT market
               illiquidity problems
             </h2>
 
-            <div className="flex flex-row pt-10">
+            <div className="flex flex-row pt-10 mr-20 w-120">
               <button
                 onClick={() => Router.push("/marketplace")}
                 className="mr-5 flex flex-row px-8 py-3 bg-mainPink text-white
           rounded-xl hover:shadow-2xl transition duration-300"
               >
-                <h3 className="ml-2 mt-2">Go To Marketplace</h3>
+                <h3 className="mt-2">Go To Marketplace</h3>
               </button>
               <button
                 onClick={() => Router.push("/create")}
                 className="mr-5 flex flex-row px-8 py-3 bg-white border border-mainPink text-gray-800
           rounded-xl hover:bg- hover:shadow-2xl transition duration-300"
               >
-                <h3 className="ml-2 mt-2">Create Lootbox</h3>
+                <h3 className="mt-2">Create Lootbox</h3>
+              </button>
+
+              <h3 className="mt-4">or</h3>
+              <button
+                onClick={() =>
+                  openInNewTab(
+                    "https://drive.google.com/file/d/1mqk53FOcxMcnHYroZ66U4F7UR022xLob/view?usp=sharing"
+                  )
+                }
+                className="ml-4 mt-2 underline"
+              >
+                <h3>Read whitepaper</h3>
               </button>
             </div>
           </div>
@@ -45,12 +58,12 @@ const Home: NextPage = () => {
 
       <section className="bg-black py-36">
         <div className="centered">
-          <AnimationOnScroll animateIn="animate__fadeInUp">
+          <AnimationOnScroll animateOnce animateIn="animate__fadeInUp">
             <div className="text-white grid grid-cols-3">
               <div className="col-span-2 text-left">
                 <h1 className="mb-4">Provable randomize lottery ticket</h1>
-                <h2 className="italic">Buy the ticket and win valuable NFT</h2>
-                <h2 className="mb-4 italic">or Create lootbox of your own NFT</h2>
+                <h2 className="italic">Buy the ticket and win the valuable NFT</h2>
+                <h2 className="mb-4 italic">or Create a lootbox of your own NFT</h2>
                 <h2 className="font-medium text-mainPink">
                   The more you BUY the more chance you WIN
                 </h2>
@@ -66,7 +79,7 @@ const Home: NextPage = () => {
       </section>
 
       <section className="py-36 text-center justify-center items-center">
-        <AnimationOnScroll animateIn="animate__fadeInRight">
+        <AnimationOnScroll animateOnce animateIn="animate__fadeInRight">
           <h1>Now its your chance to win valuable NFTs</h1>
           <div className="w-full h-120 -py-100 border-red-400">
             <NFTShowcase />
@@ -76,52 +89,69 @@ const Home: NextPage = () => {
 
       <section className="bg-black py-36">
         <div className="centered">
-          <AnimationOnScroll animateIn="animate__fadeInLeft">
+          <AnimationOnScroll animateOnce animateIn="animate__fadeInLeft">
             <h1 className="text-white">Core value</h1>
-            <div className="text-white grid grid-cols-2 gap-20 mt-10">
-              <Card text="Solve illiquidity problems" />
-              <Card text="Gamification experiences" />
+            <div className="text-white grid grid-cols-3 gap-10 mt-10">
+              <Card pictureURL="/Normal_Tickets.png" />
+              <Card pictureURL="/Winning_Ticket.png" />
+              <Card pictureURL="/Refundable_Ticket.png" />
+              <Card pictureURL="/Refunded_Ticket.png" />
+              <Card pictureURL="/Claimed_Ticket.png" />
+              <Card pictureURL="/Expired_Ticket.png" />
             </div>
           </AnimationOnScroll>
         </div>
       </section>
 
-      <section className="py-36 centered">
-        <AnimationOnScroll animateIn="animate__fadeInRight">
-          <div className="">
-            <h1>Roadmap</h1>
-            <div className="flex items-center my-8">
-              <img src="/Roadmap/roadmap.png" alt="roadmap" className="w-4/5 m-auto" />
-            </div>
-          </div>
-        </AnimationOnScroll>
-      </section>
+      <section className="bg-white py-72"></section>
 
-      <section className="py-36 bg-black">
-        <AnimationOnScroll animateIn="animate__fadeInUp">
-          <div className="centered">
+      <section className="py-28 bg-black">
+        <AnimationOnScroll animateOnce animateIn="animate__fadeInUp">
+          <div className="centered bg-black">
             <h1 className="text-white">Available Now On Testnet</h1>
             <div className="pt-20 grid grid-cols-3 gap-10">
               <div className="flex flex-col items-center justify-center">
                 <Illustration logo="binance" />
-                {/* <img width={100} height={100} src="/chain/BNB.png" alt="BNBChain" />
-              <h3 className="pt-4">Binance</h3> */}
               </div>
 
               <div className="flex flex-col items-center justify-center">
                 <Illustration logo="polygon" />
-                {/* <img width={100} height={100} src="/chain/Polygon.png" alt="PolygonChain" />
-              <h3 className="pt-4">Polygon</h3> */}
               </div>
               <div className="flex flex-col items-center justify-center">
                 <Illustration logo="avalanche" />
-                {/* <img width={100} height={100} src="/chain/Avalanche.png" alt="EthChain" />
-              <h3 className="pt-4">Avalanche</h3> */}
               </div>
             </div>
           </div>
         </AnimationOnScroll>
       </section>
+
+      <section className="py-36 centered">
+        <AnimationOnScroll animateOnce animateIn="animate__fadeInRight">
+          <div className="">
+            <h1>Roadmap</h1>
+            <div className="flex items-center my-8">
+              <img src="/roadmap/roadmap2.png" alt="roadmap" className="w-4/5 m-auto" />
+            </div>
+          </div>
+        </AnimationOnScroll>
+      </section>
+
+      <section className="py-28 bg-black">
+        <AnimationOnScroll animateOnce animateIn="animate__fadeInUp">
+          <div className="centered">
+            <h1 className="text-white mb-20">Powered by</h1>
+
+            <div className="flex flex-row items-center ">
+              <img className="w-24 mr-20" alt="sponsor" src="/sponsor/chainlinkwhite.png" />
+              <img className="w-56 mr-20" alt="sponsor" src="/sponsor/moraliswhite.png" />
+              <img className="w-24 mr-20" alt="sponsor" src="/sponsor/ipfswhite.png" />
+              <img className="w-48 mr-20" alt="sponsor" src="/sponsor/thegraphwhite.png" />
+              <img className="w-24" alt="sponsor" src="/sponsor/filecoinwhite.png" />
+            </div>
+          </div>
+        </AnimationOnScroll>
+      </section>
+      {/* <section className="bg-white py-36"></section> */}
     </div>
   )
 }
